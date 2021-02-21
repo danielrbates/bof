7615d59ed7c143d91a270ef9d92770409ba3fb27
Daniel Bates <danielrbates@gmail.com>
2021-02-20 23:01:50 -0500

    Update fuzzer.py

--- fuzzer.py
+++ fuzzer.py
@@ -2,10 +2,13 @@
 
 import socket, time, sys
 
-ip = "10.1.1.1"
-port = 1337
-timeout = 5
+# Set up connection
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.settimeout(timeout)
+connect = s.connect((ip,port))
+s.recv(1024)
 
+# Iterate fuzzing
 buffer = []
 counter = 100
 while len(buffer) < 30:
@@ -14,15 +17,13 @@ while len(buffer) < 30:
 
 for string in buffer:
     try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(timeout)
-        connect = s.connect((ip, port))
-        s.recv(1024)
         print("Fuzzing with %s bytes" % len(string))
         s.send(string + "\r\n")
         s.recv(1024)
-        s.close()
     except:
         print("Could not connect to " + ip + ":" + str(port))
         sys.exit(0)
     time.sleep(1)
+
+finally:
+        s.close()
